0e33c9b723281f8317eb904767898d9d99999c7c
Tomáš Kasl <kasltoma@fel.cvut.cz>
2019-12-18 17:54:58 +0100

    network code for wheel added

---
+++ wheel/network.c
@@ -0,0 +1,44 @@
+#include <inetLib.h>
+#include <sockLib.h>
+
+#define TARGET_IP "192.168.202.242"
+#define MANAGEMENT_PORT 42420
+
+struct sockaddr_in my_addr, target_addr;
+int sockd, addrlen;
+struct timeval tval;
+
+network_init(){
+    tval.tv_sec = 0;
+    tval.tv_usec = 1000000;
+
+    if ((sockd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        printf("Socket creation error\n");
+        return;
+    }
+
+    memset(&target_addr, 0, sizeof(target_addr));
+    target.sin_family = AF_INET;
+    target.sin_addr.s_addr = inet_addr(TARGET_IP);
+    target.sin_port = htons(MANAGEMENT_PORT);
+
+    memset(&my_addr, 0, sizeof(my_addr));
+    src.sin_family = AF_INET;
+    src.sin_addr.s_addr = INADDR_ANY;
+    src.sin_port = htons(MANAGEMENT_PORT);
+
+    int yes=1; 
+    if (setsockopt(sockd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        printf("failed to set reuse\n");
+        exit(1);
+    }
+
+    if (setsockopt(sockd, SOL_SOCKET, SO_RCVTIMEO,&tval,sizeof(tval)) < 0) {
+        printf("failed to set timeout\n");
+    }
+
+    if (bind(sockd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1) {
+        printf("failed to bind socket \n");
+        exit(1);
+    }
+}
