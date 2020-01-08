71c897898e4b6b028a69281c0c455ff6d011569f
Tomas Kasl <t.kasl@sap.com>
2020-01-08 17:26:24 +0100

    obsolete data from network.h deleted

--- tire/network.h
+++ tire/network.h
@@ -17,12 +17,10 @@
 #include <taskLib.h>
 #include "global.h"
 
-#define BASE_DUTY 0xB0/64
 #define SERVER_PORT     80 /* Port 80 is reserved for HTTP protocol */
 #define SERVER_MAX_CONNECTIONS  20
 #define MANAGEMENT_PORT 42420
-#define WEBPAGE "webpage.html"
-#define PRIORITY 100
+#define PRIORITY 5
 #define ifreq_offsetof(x)  offsetof(struct ifreq, x)
 
 short* PosHistory;
@@ -32,12 +30,6 @@ short* DesPosHistorySWAP;
 short* PWMHistory;
 short* PWMHistorySWAP;
 unsigned timemark = 0;
-
-int FINISHED = 0;
-int sockd;
-int yes = 1;
-struct sockaddr_in serverAddr, src, mngAddr, my_addr;
-struct timeval tval;
 
 /** function called by thread responsible for updating web server */
 void www();
