73b1c82d64384b7bc6b18781e37c6d71da34e635
Tomas Kasl <t.kasl@sap.com>
2020-01-08 13:34:58 +0100

    data for plotting are now saved and used

--- tire/network.c
+++ tire/network.c
@@ -6,7 +6,7 @@ short* DesPosHistory;
 short* DesPosHistorySWAP;
 short* PWMHistory;
 short* PWMHistorySWAP;
-short timemark = 0;
+unsigned timemark = 0;
 
 int FINISHED = 0;
 struct sockaddr_in serverAddr,src,mngAddr;
@@ -63,7 +63,6 @@ void www()
       close(s);
       return;
     }
-    
     /* The client connected from IP address inet_ntoa(clientAddr.sin_addr)
        and port ntohs(clientAddr.sin_port).
 
@@ -71,73 +70,64 @@ void www()
        and sends back the response.
 
        Don't forget to close newFd at the end */
-    int service = taskSpawn("service", PRIORITY+2, 0, 4096, (FUNCPTR)serve, newFd, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    taskSpawn("service", PRIORITY+2, 0, 4096, (FUNCPTR)serve, newFd, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     }
 }
 
 void serve(int fd) {
+	printf("Serving a HTML page\n");
     FILE *tunnel = fdopen(fd, "w");
     fprintf(tunnel, "HTTP/1.0 200 OK\r\n\r\n");
-    //fprintf(tunnel, "Current time is %ld.\n");
-    int chr;
-   // load html file
-    
-    FILE *source = fopen(WEBPAGE, "r");
-    if(source == NULL){
-        perror("incorrect html source");
-    }
-   // send all his contents
+    fprintf(tunnel, "<!DOCTYPE HTML>\n<html style='background-color: yellowgreen;'>\n<head>\n  <h1 align='center' style='color: red; font-size: 9pc;'><u>Motion Ownage</u></h1>\n  <h3 style='color: red; font-size: 2pc;'>Graphs:</h3>\n<script type='text/javascript' id='myHtml'>\nsetTimeout(function(){location.reload()}, 1000);\nwindow.onload = function () {\n  var chart = new CanvasJS.Chart('chartContainer', {\n	animationEnabled: false,  \n	title:{\n		text: 'Motor position'\n	},\n	axisY: {\n		title: 'Position',\n		valueFormatString: '#0,,.',\n		suffix: '',\n	},\n	data: [{\n    name: 'Desired position',\n    showInLegend: true,\n		yValueFormatString: '#,### Units',\n		xValueFormatString: '####',\n		type: 'spline',\n		dataPoints: [ //&\n		]\n	},\n  {\n    name: 'Actual position',\n    showInLegend: true,\n		yValueFormatString: '#,### Units',\n		xValueFormatString: '####',\n		type: 'spline',\n		dataPoints: [");
     int i;
-    while( (chr = fgetc(source) ) != EOF) {
-        switch(chr) {
-          case '&':
-            fputc('\n',tunnel);
-            for(i=0; i<timemark; i++) {
-                fprintf(tunnel, "{x: %d, y: %d},",i, PosHistory[i]);
-            } break;
-          case '@':
-            fputc('\n',tunnel);
-            for(i=0; i<timemark; i++) {
-                fprintf(tunnel, "{x: %d, y: %d},",i, DesPosHistory[i]);
-            } break;
-          case '$':
-            fputc('\n',tunnel);
-            for(i=0; i<timemark; i++) {
-                fprintf(tunnel, "{x: %d, y: %d},",i, PWMHistory[i]);
-            } break;
-          default:
-            fputc(chr, tunnel);
-        }
-    }
-    fclose(source);
+    for(i=0; i<timemark; i++) {
+        fprintf(tunnel, "{x: %d, y: %d},",i,PosHistory[i]);
+    };
+    fprintf(tunnel, "]\n	},\n  {\n    name: 'Desired position',\n    showInLegend: true,\n		yValueFormatString: '#,### Units',\n		xValueFormatString: '####',\n		type: 'spline',\n		dataPoints: [");
+    for(i=0; i<timemark; i++) {
+        fprintf(tunnel, "{x: %d, y: %d},",i,DesPosHistory[i]);
+    };
+    fprintf(tunnel, "]	}]});chart.render();\nvar chart2 = new CanvasJS.Chart('chartContainer2', {\n	animationEnabled: false,  \n	title:{\n		text: 'PMW level'\n	},\n	axisY: {\n		title: 'PMW level',\n		valueFormatString: '#0,,.',\n		suffix: '',\n	},\n	data: [{\n    name: 'PMW',\n		yValueFormatString: '#,### Units',\n		xValueFormatString: '####',\n		type: 'spline',\n		dataPoints: [");
+    for(i=0; i<timemark; i++) {
+    	fprintf(tunnel, "{x: %d, y: %d},",i, PWMHistory[i]);
+    };
+    fprintf(tunnel, "]\n	}]\n});\nchart2.render();\n}\n</script>\n</head>\n<body>\n<div align='center' id='chartContainer' style='height: 370px; width: 97%; margin: 2pc'></div>\n<div id='chartContainer2' style='height: 370px; width: 97%; margin: 2pc;'></div>\n<script src='https://canvasjs.com/assets/script/canvasjs.min.js'> </script>\n\n<footer style='font-size: 1pc;'>\n  By Tomas Kasl and Zdenek Syrovy<br>\n  FEL CTU 2020\n</footer>\n</body>\n</html>\n");
+    printf("serving done\n");
     fclose(tunnel);
 }
 
 void initreceive(){
-	    if ((sockd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-	        printf("Socket creation error\n");
-	        return;
-	    }
-	    memset(&my_addr, 0, sizeof(my_addr));
-	    my_addr.sin_family = AF_INET;
-	    my_addr.sin_addr.s_addr = INADDR_ANY;
-	    my_addr.sin_port = htons(MANAGEMENT_PORT);
-	    memset(&(my_addr.sin_zero), '\0', 8);
-	    int yes=1; 
-	    if (setsockopt(sockd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-	        printf("failed to set reuse\n");
-	        return;
-	    }
-	    tval.tv_usec = 1000000;
-	    if (setsockopt(sockd, SOL_SOCKET, SO_RCVTIMEO,&tval,sizeof(tval)) < 0) {
-	        printf("failed to set timeout\n");
-	        return;
-	    }
+    PosHistory = malloc(2000*sizeof(short));
+    PosHistorySWAP = malloc(2000*sizeof(short));
+    DesPosHistory = malloc(2000*sizeof(short));
+    DesPosHistorySWAP = malloc(2000*sizeof(short));
+    PWMHistory = malloc(2000*sizeof(short));
+    PWMHistorySWAP = malloc(2000*sizeof(short));
 
-	    if (bind(sockd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1) {
-	        printf("failed to bind socket \n");
-	        return;
- }
+    if ((sockd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        printf("Socket creation error\n");
+        return;
+    }
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    my_addr.sin_port = htons(MANAGEMENT_PORT);
+    memset(&(my_addr.sin_zero), '\0', 8);
+    int yes=1; 
+    if (setsockopt(sockd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        printf("failed to set reuse\n");
+        return;
+    }
+    tval.tv_usec = 10000;
+    if (setsockopt(sockd, SOL_SOCKET, SO_RCVTIMEO,&tval,sizeof(tval)) < 0) {
+        printf("failed to set timeout\n");
+        return;
+    }
+
+    if (bind(sockd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1) {
+        printf("failed to bind socket \n");
+        return;
+    }
 }
 
 void connectionListener(){
@@ -147,41 +137,41 @@ void connectionListener(){
     printf("in answererTask\n");
     ssize_t status;
     while(!FINISHED){
-    	//printf("waiting for packet\n");
         status = recvfrom(sockd,buffer,11,0,(struct sockaddr*)&src,&srclen);
-        if(status < 0){
-            //printf("ERROR: %s\n", strerror(errno));
+        if(status < 0){//timed-out
             continue;
         }
         if (buffer[0] == 'P' && buffer[1] == 'W' && buffer[2] == 'N' && buffer[3] == 'S' && buffer[4] == 'T' && buffer[5] == 'N') {
-            //TODO musime to poslat do PIDčka
             memcpy(&desiredPosition, buffer+6, 4);
-            //printf("prijat desiredPosition %d\n", desiredPosition);
-           
-            /* 
+            /**
+             * save the measured data into data containers used for plotting
+            */
             PosHistory[timemark] = position;
             DesPosHistory[timemark] = desiredPosition;
-            //TODO speed * +- direction
-            PWMHistory[timemark] = speed;
+            //left or right
+            if (direction > 1)
+                PWMHistory[timemark] = -speed;
+            else	
+                PWMHistory[timemark] = speed;
+            
             if(timemark > 999){
-                PosHistorySWAP[timemark-999] = position;
-                DesPosHistory[timemark-999] = desiredPosition;
-                //TODO speed * +- direction
-                PWMHistory[timemark-999] = speed;
-            }
-            if(timemark == 1998) {
-              timemark = 998;
+                PosHistorySWAP[timemark-1000] = position;
+                DesPosHistory[timemark-1000] = desiredPosition;
+                if (direction > 1)
+                	PWMHistory[timemark-1000] = -speed;
+                else
+                	PWMHistory[timemark-1000] = speed; 
+        	}  		
+            if(timemark == 1999) { //we got to the end of the container, we need to swap them
+              timemark = 999;
               short *tmpptr = PosHistory;
               PosHistory = PosHistorySWAP;
-
               tmpptr = DesPosHistory;
-              PosHistory = DesPosHistorySWAP;
-
+              DesPosHistory = DesPosHistorySWAP;
               tmpptr = PWMHistory;
-              PosHistory = PWMHistorySWAP;
+              PWMHistory = PWMHistorySWAP;
             }
             timemark++;
-            */
         }
     }
     return;
