1ce5521b906e133d6cfe1647d27f5e44aa5b48af
Zdeněk Syrový <syrovzde@fel.cvut.cz>
2020-01-09 19:56:15 +0100

    Updated irc in wheel

--- wheel/motor.c
+++ wheel/motor.c
@@ -6,7 +6,7 @@
  * interrupt handler, checkes which way engine rotated and sets global position accordingly.
  * 
  */
-void motorWatcher() {
+/*void motorWatcher() {
         a = (MOTOR_SR & BIT(MOTOR_SR_IRC_A_MON)) != 0;
         b = (MOTOR_SR & BIT(MOTOR_SR_IRC_B_MON)) != 0;
 
@@ -42,8 +42,40 @@ void motorWatcher() {
         prev_a = a;
         prev_b = b;
         irq_count++;
-        GPIO_INT_STATUS = MOTOR_IRQ_PIN; /* clear the interrupt */
+        GPIO_INT_STATUS = MOTOR_IRQ_PIN; /* clear the interrupt 
+}*/
+ static const int table[16] = {
+          [TRANSITION(0,1)] = +1,
+          [TRANSITION(1,3)] = +1,
+          [TRANSITION(3,2)] = +1,
+          [TRANSITION(2,0)] = +1,
+          [TRANSITION(1,0)] = -1,
+          [TRANSITION(3,1)] = -1,
+          [TRANSITION(2,3)] = -1,
+          [TRANSITION(0,2)] = -1,
+  };
+void motorWatcher()
+{
+    int irc_state = (MOTOR_SR & BIT(MOTOR_SR_IRC_A_MON) ? 1 : 0) | (MOTOR_SR & BIT(MOTOR_SR_IRC_B_MON) ? 2 : 0);
+    position += table[TRANSITION(last_irc_state, irc_state)];
+    last_irc_state = irc_state;
+    GPIO_INT_STATUS = MOTOR_IRQ_PIN; /* clear the interrupt */
 }
+/**
+ * initiation of all needed constants and registers, connecting interrupt hanlder and enabling of all GPIO interrupts
+ */
+void motorWatcherInit() {
+        GPIO_INT_STATUS = MOTOR_IRQ_PIN; /* reset status */
+        GPIO_DIRM = 0x0;                 /* set as input */
+        GPIO_INT_TYPE = MOTOR_IRQ_PIN;   /* interrupt on edge */
+        GPIO_INT_POLARITY = 0x0;         /* rising edge */
+        GPIO_INT_ANY = 0x0;              /* ignore falling edge */
+        GPIO_INT_ENABLE = MOTOR_IRQ_PIN; /* enable interrupt on MOTOR_IRQ pin */
+        position = 0;
+        intConnect(INUM_TO_IVEC(INT_LVL_GPIO), motorWatcher, 0);
+        intEnable(INT_LVL_GPIO);         /* enable all GPIO interrupts */
+}
+
 /**
  * initiation of all needed constants and registers, connecting interrupt hanlder and enabling of all GPIO interrupts
  */
